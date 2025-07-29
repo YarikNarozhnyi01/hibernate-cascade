2c8cfa77cfec45bfdc70ff3686afb299d0144886
Yaroslav Narozhnyi <gnredit9@gmail.com>
2025-07-29 12:49:15 +0300

    impoer changes

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -3,11 +3,11 @@ package core.basesyntax.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
 import java.util.List;
 
 @Entity
